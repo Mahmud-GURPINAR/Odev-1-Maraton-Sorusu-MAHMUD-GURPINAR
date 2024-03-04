a209fe60ee6c317a96172f71143d9241f5423527
Mahmud-GURPINAR <2023123352@cumhuriyet.edu.tr>
2024-03-04 19:32:18 +0300

    Odev 1 Maraton Sorusu MAHMUD GURPINAR
    NESNE TABANLI PROGRAMLAMA LAB.
    UYGULAMA ÖDEVİ
    1 MAHMUD GÜRPINAR

--- Ödev 1 Maraton Sorusu MAHMUD GÜRPINAR/Marathon.java
+++ Ödev 1 Maraton Sorusu MAHMUD GÜRPINAR/Marathon.java
@@ -47,7 +47,7 @@ public class Marathon {
     }
 
     public int second() { //En düşük ikinci süreye sahip koşucuyu bulup onun dizideki sırasını dönderen method.
-        int secondTime = time[0], count2 = 0;
+        int secondTime = 99999, count2 = 0; // seconTimeye  bu değeri girme sebebim yüksek değerli olması gerekmesi çünkü 0. indexdeki değer en küçük sayı olursa hiçbir zaman if döngüsüne girmez hatalı sonuç çıkar.
 
         for (int i = 0; i < time.length; i++) {
             if (time[i] < secondTime && time[i] > time[first()]) { //Koşul eğer yeni sayı ikinci en küçükten küçük ve sayı birinci methodda bulunan sıradaki süreden büyükse çalışıyor.
@@ -59,7 +59,7 @@ public class Marathon {
     }
 
     public int third() { //En düşük üçüncü süreye sahip koşucuyu bulup onun dizideki sırasını dönderen method.
-        int thirdTime = time[0], count3 = 0;
+        int thirdTime = 99999, count3 = 0; // thirdTimeye  bu değeri girme sebebim yüksek değerli olması gerekmesi çünkü 0. indexdeki değer en küçük sayı olursa hiçbir zaman if döngüsüne girmez hatalı sonuç çıkar.
 
         for (int i = 0; i < time.length; i++) {
             if (time[i] < thirdTime && time[i] > time[second()]) { //Koşul eğer yeni sayı üçüncü en küçükten küçük ve sayı ikinci methodda bulunan sıradaki süreden büyükse çalışıyor.
--- Ödev 1 Maraton Sorusu MAHMUD GÜRPINAR/MarathonTest.java
+++ Ödev 1 Maraton Sorusu MAHMUD GÜRPINAR/MarathonTest.java
@@ -1,8 +1,10 @@
 public class MarathonTest {
     public static void main(String[] args) {
         int[] timeArray = {341,273,278,329,445,402,388,270,243,334,412,393,299,343,317,265}; //Koşucuların sürelerinin bulunduğu dizi.
-        String[] nameArray = {"Kadir 341'","Gökhan 273'","Hakan 278'","Suzan 329'","Pınar 445'","Mehmet 402'","Ali 388'","Emel 270'", //Koşucuların isimlerinin ve sürelerinin olduğu dizi.
-                "Fırat 243'","James 334'","Jale 412'","Ersin 393'","Deniz 299'","Gözde 343'","Anıl 317'","Burak 265'"};
+        String[] nameArray = {"Kadir "+timeArray[0]+"'","Gökhan "+timeArray[1]+"'","Hakan "+timeArray[2]+"'","Suzan "+timeArray[3]+"'","Pınar "+timeArray[4]+"'","Mehmet "+timeArray[5]+"'",
+                "Ali "+timeArray[6]+"'","Emel "+timeArray[7]+"'","Fırat "+timeArray[8]+"'","James "+timeArray[9]+"'","Jale "+timeArray[10]+"'","Ersin "+timeArray[11]+"'",
+                "Deniz "+timeArray[12]+"'","Gözde "+timeArray[13]+"'","Anıl "+timeArray[14]+"'","Burak "+timeArray[15]+"'"}; //Koşucuların isimlerinin ve sürelerinin olduğu dizi.
+                // Direk Stringin içine "Kadir 341'" yazmak yerine +timeArray şeklinde yazma sebebim süre dizisinde bir süreyi değiştirmek istendiği zaman iki işlem yerine tek işlemin yeterli olmassı
 
 
         Marathon myMarathon = new Marathon(timeArray, nameArray);
